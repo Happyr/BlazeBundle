491321499afa95358381c7708cfcc244833be804
Tobias Nyholm <tobias.nyholm@gmail.com>
2015-12-23 22:00:00 +0100

    Formated the lines better

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,45 +18,44 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('happyr_blaze')
-
-            ->children()
+        $treeBuilder->root('happyr_blaze')->children()
             ->arrayNode('objects')
-            ->requiresAtLeastOneElement()
-            ->useAttributeAsKey('name')
-            ->prototype('variable')
-            ->treatNullLike(array())
-            //make sure that there is some config after each object
-            ->validate()
-            ->ifTrue(
-                function ($objects) {
-                    foreach ($objects as $o) {
-                        if (!is_array($o)) {
-                            return true;
+                ->requiresAtLeastOneElement()
+                ->useAttributeAsKey('name')
+                ->prototype('variable')
+                ->treatNullLike(array())
+
+                //make sure that there is some config after each object
+                ->validate()
+                    ->ifTrue(
+                        function ($objects) {
+                            foreach ($objects as $o) {
+                                if (!is_array($o)) {
+                                    return true;
+                                }
+                            }
+
+                            return false;
                         }
-                    }
+                    )
+                    ->thenInvalid('The happyr_blaze.objects config %s must be an array.')
+                ->end()
 
-                    return false;
-                }
-            )
-            ->thenInvalid('The happyr_blaze.objects config %s must be an array.')
-            ->end()
-
-            //make sure route and parameters is set
-            ->validate()
-            ->ifTrue(
-                function ($objects) {
-                    foreach ($objects as $o) {
-                        if (!isset($o['route']) || !isset($o['parameters'])) {
-                            return true;
+                //make sure route and parameters is set
+                ->validate()
+                ->ifTrue(
+                    function ($objects) {
+                        foreach ($objects as $o) {
+                            if (!isset($o['route']) || !isset($o['parameters'])) {
+                                return true;
+                            }
                         }
-                    }
 
-                    return false;
-                }
-            )
-            ->thenInvalid('%s must contain "route" and "parameters".')
-            ->end()
+                        return false;
+                    }
+                )
+                ->thenInvalid('%s must contain "route" and "parameters".')
+                ->end()
             ->end()
             ->end();
 
